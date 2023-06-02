cdf985c907538d8ac4017b06aef11a57d2d3b277
Claire Siroky <claire.siroky@utexas.edu>
2023-06-02 18:18:37 -0500

    undo prev commit

--- examples/main.js
+++ examples/main.js
@@ -10,8 +10,8 @@ import   load_mujoco        from '../dist/mujoco_wasm.js';
 const mujoco = await load_mujoco();
 
 // Set up Emscripten's Virtual File System
-var initialScene = "robosuite/models/assets/demonstrations/wipe/panda/models/model_1.xml";
-//var initialScene = "humanoid.xml";
+//var initialScene = "robosuite/models/assets/demonstrations/wipe/panda/models/model_1.xml";
+var initialScene = "humanoid.xml";
 mujoco.FS.mkdir('/working');
 mujoco.FS.mount(mujoco.MEMFS, { root: '.' }, '/working');
 mujoco.FS.writeFile("/working/" + initialScene, await(await fetch("./examples/scenes/" + initialScene)).text());
