254142eeae72e69825558319383066e88d911b32
Claire Siroky <claire.siroky@utexas.edu>
2023-06-06 21:06:49 -0500

    try

--- examples/main.js
+++ examples/main.js
@@ -15,16 +15,16 @@ var initialScene = "humanoid.xml";
 mujoco.FS.mkdir('/working');
 mujoco.FS.mount(mujoco.MEMFS, { root: '.' }, '/working');
 //mujoco.FS.writeFile("/working/" + initialScene, await(await fetch("./examples/scenes/" + initialScene)).text());
-await downloadExampleScenesFolder(mujoco);
+//await downloadExampleScenesFolder(mujoco);
 
 export class MuJoCoDemo {
   constructor() {
     this.mujoco = mujoco;
 
     // Load in the state from XML
-    this.model      = new mujoco.Model("/working/" + initialScene);
-    this.state      = new mujoco.State(this.model);
-    this.simulation = new mujoco.Simulation(this.model, this.state);
+    //this.model      = new mujoco.Model("/working/" + initialScene);
+    //this.state      = new mujoco.State(this.model);
+    //this.simulation = new mujoco.Simulation(this.model, this.state);
 
     // Define Random State Variables
     this.params = { scene: initialScene, paused: false, help: false, ctrlnoiserate: 0.0, ctrlnoisestd: 0.0, keyframeNumber: 0 };
@@ -78,7 +78,7 @@ export class MuJoCoDemo {
 
   async init() {
     // Download the the examples to MuJoCo's virtual file system
-    //await downloadExampleScenesFolder(mujoco);
+    await downloadExampleScenesFolder(mujoco);
 
     // Initialize the three.js Scene using the .xml Model in initialScene
     [this.model, this.state, this.simulation, this.bodies, this.lights] =  
