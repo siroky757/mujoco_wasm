c918ede7b1b447b571101a4a3fd925068553d19a
Claire Siroky <claire.siroky@utexas.edu>
2023-06-07 17:41:52 -0500

    trying again

--- examples/main.js
+++ examples/main.js
@@ -14,8 +14,8 @@ var initialScene = "wipe_panda/wipe_panda_model_1.xml";
 //var initialScene = "humanoid.xml";
 mujoco.FS.mkdir('/working');
 mujoco.FS.mount(mujoco.MEMFS, { root: '.' }, '/working');
-mujoco.FS.writeFile("/working/" + initialScene, await(await fetch("./examples/scenes/" + initialScene)).text());
-//await downloadExampleScenesFolder(mujoco);
+//mujoco.FS.writeFile("/working/" + initialScene, await(await fetch("./examples/scenes/" + initialScene)).text());
+await downloadExampleScenesFolder(mujoco);
 
 export class MuJoCoDemo {
   constructor() {
@@ -78,7 +78,7 @@ export class MuJoCoDemo {
 
   async init() {
     // Download the the examples to MuJoCo's virtual file system
-    await downloadExampleScenesFolder(mujoco);
+    //await downloadExampleScenesFolder(mujoco);
 
     // Initialize the three.js Scene using the .xml Model in initialScene
     [this.model, this.state, this.simulation, this.bodies, this.lights] =  
