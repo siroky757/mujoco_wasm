ace13cf469130f9c983c14196717e60d32b8c7a7
Claire Siroky <claire.siroky@utexas.edu>
2023-06-07 19:19:35 -0500

    trying writeFIle humanoid

--- examples/main.js
+++ examples/main.js
@@ -10,8 +10,8 @@ import   load_mujoco        from '../dist/mujoco_wasm.js';
 const mujoco = await load_mujoco();
 
 // Set up Emscripten's Virtual File System
-var initialScene = "wipe_panda/wipe_panda_model_1.xml";
-//var initialScene = "humanoid.xml";
+//var initialScene = "wipe_panda/wipe_panda_model_1.xml";
+var initialScene = "humanoid.xml";
 mujoco.FS.mkdir('/working');
 mujoco.FS.mount(mujoco.MEMFS, { root: '.' }, '/working');
 mujoco.FS.writeFile("/working/" + initialScene, await(await fetch("./examples/scenes/" + initialScene)).text());
